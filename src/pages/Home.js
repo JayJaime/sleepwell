46e09f53cf2e406a08f7013f4643ecf607b6ed46
JayJaime <jj1998april@gmail.com>
2022-03-27 22:27:15 -0600

    added mt to video

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -112,9 +112,9 @@ function Home() {
 
       {/* How It Works Section */}
       <div className="mt-[24px] w-full py-[64px] lg:py-[96px] flex items-center justify-center px-[24px] md:px-[56px]">
-        <div className="max-w-[1280px] w-full flex flex-col-reverse lg:flex-row items-center justify-center gap-[40px]">
+        <div className="max-w-[1280px] w-full flex flex-col-reverse lg:flex-row items-center justify-center gap-[40px] lg:gap-[40px]">
           {/* video */}
-          <div className="aspect-video overflow-hidden rounded-xl max-w-[640px] w-full relative">
+          <div className="aspect-video overflow-hidden rounded-xl max-w-[640px] w-full relative mt-[40px]">
             <div id="poster" onClick={playVideo}>
               <img
                 className="absolute left-0 right-0 top-0 bottom-0 m-auto cursor-pointer"
