0b6ba591f95688e51f2c890f51f92e1317e05b86
JayJaime <jj1998april@gmail.com>
2022-05-11 22:29:07 -0600

    track page views ga

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
-import React, { useLayoutEffect } from "react";
+import React, { useLayoutEffect, useEffect } from "react";
 import { Routes, Route } from "react-router-dom";
+import ReactDOM from "react-dom";
+import ReactGA from "react-ga";
 import Home from "./pages/Home";
 import Cost from "./pages/Cost";
 import About from "./pages/About";
@@ -8,11 +10,18 @@ import Schedule from "./pages/Schedule";
 import Education from "./pages/Education";
 import Nav from "./components/Navigation";
 import "./App.css";
+const MEASUREMENT_ID = "G-MS39CCGTTQ"; // OUR_TRACKING_ID
+
+ReactGA.initialize(TRACKING_ID);
 
 function App() {
   useLayoutEffect(() => {
     window.scrollTo(0, 0);
   });
+
+  useEffect(() => {
+    ReactGA.pageview(window.location.pathname + window.location.search);
+  }, []);
 
   return (
     <div className="App">
